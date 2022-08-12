ae494dbb7cad5e83b17c5a63a765a2f5f7eec4b1
Gerardo Aguilar <gerardo.aguilar@unosquare.com>
2022-08-11 21:56:36 -0500

    Added Models

---
+++ ToysAndGames.Models/Models/Company.cs
@@ -0,0 +1,8 @@
+﻿namespace ToysAndGames.Domain.Models
+{
+   public class Company
+   {
+      public int Id { get; set; }
+      public string Name { get; set; }
+   }
+}
---
+++ ToysAndGames.Models/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ToysAndGames.Domain.Models
+{
+   public class Product
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public int AgeRestriction { get; set; }
+      public Company Company { get; set; }
+      public int CompanyId { get; set; }
+      public decimal Price { get; set; }
+   }
+}
