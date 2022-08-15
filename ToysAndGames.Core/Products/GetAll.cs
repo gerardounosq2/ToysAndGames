651135971453f6181432aa716a9074dda8ecd366
Gerardo Aguilar <gerardo.aguilar@unosquare.com>
2022-08-15 10:07:57 -0500

    Added MediatR GetAll Products command and handler

---
+++ ToysAndGames.Core/Products/GetAll.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using ToysAndGames.Domain.Models;
+using ToysAndGames.Persistence.Repository;
+
+namespace ToysAndGames.Core.Products
+{
+   public class GetAll
+   {
+      public class Command : IRequest<ICollection<Product>>
+      {
+
+      }
+
+      public class GetAllCommandHandler : IRequestHandler<Command, ICollection<Product>>
+      {
+         readonly IAsyncRepository<Product> productRepo;
+
+         public GetAllCommandHandler(IAsyncRepository<Product> productRepo)
+         {
+            this.productRepo = productRepo;
+         }
+
+         public async Task<ICollection<Product>> Handle(Command request, CancellationToken cancellationToken)
+         {
+            return await productRepo.GetAllAsync();
+         }
+      }
+   }
+}
